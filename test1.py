a8647088fcb5ef5dd56e05dfad9d37a94919211e
Yilong Yang <ynulonger@yahoo.com>
2017-12-06 20:29:55 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,9 +4,3 @@ import matplotlib.pyplot as plt
 import Kmeans
 dataSet=Kmeans.loadDataSet("testSet2.txt")
 myCentroids,clustAssing = Kmeans.kmeans(dataSet,3)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(list(dataSet[:,0]), list(dataSet[:,1]), marker='^', s=90)
-ax.scatter(list(myCentroids[:,0]),list(myCentroids[:,1]),marker='+')
-plt.show()
